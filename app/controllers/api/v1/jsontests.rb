2882a3316188dad5708846d60302158bdd3614b6
michpolak <polak2@kenyon.edu>
2023-06-27 17:23:54 +0000

    Basic JSON Test GET by ID API

--- app/controllers/api/v1/jsontests.rb
+++ app/controllers/api/v1/jsontests.rb
@@ -4,9 +4,15 @@ module API
       include API::V1::Defaults
 
       resource :jsontests do
-		route :get, 'hello' do 
-			Jsontest.all
-		end
+		#route :get, 'hello' do 
+		#	Jsontest.all
+		#end
+		params do
+			requires :id, type: String, desc: "ID of the wificlient"
+        end
+		get ":id", root:"jsontests" do
+			Jsontest.where(id: permitted_params[:id])
+        end
       end
 	  
 	  
