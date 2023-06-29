c890bf453cff7c29450487162cb93e6636bd5fc8
James Skon <skonjp@kenyon.edu>
2023-06-29 20:06:39 +0000

    Fix json in test endpint

--- app/controllers/api/v1/jsontests.rb
+++ app/controllers/api/v1/jsontests.rb
@@ -1,19 +1,22 @@
+require 'json'
 module API
   module V1
     class Jsontests < Grape::API
       include API::V1::Defaults
 
       resource :jsontests do
-		#route :get, 'hello' do 
+	        #route :get, 'hello' do 
 		#	Jsontest.all
 		#end
-		params do
+	        params do
 			requires :id, type: String, desc: "ID of the wificlient"
         end
-		get ":id", root:"jsontests" do
-			JSON.parse(Jsontest.find_by(id: permitted_params[:id]).config_json)
-			
+		post ":id", root:"jsontests" do
+			render json: JSON.parse(Jsontest.find_by(id: permitted_params[:id]).config_json)
         end
+        puts "JSONTESTS"
+        puts "PARAMS #{params}"
+			
       end
 	  
 	  
