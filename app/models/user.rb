eacd54d8239795bd3735af31ba60d5b7a1f7421c
michpolak <polak2@kenyon.edu>
2023-07-03 17:16:27 +0000

    Fixed Destroy User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 	#attr_accessor :zone_id, :name
 	belongs_to :manager, optional: true
-	has_and_belongs_to_many :zones
+	#has_and_belongs_to_many :zones
+	has_many :zones
+	belongs_to :zones
 end
--- app/models/zone.rb
+++ app/models/zone.rb
@@ -1,5 +1,7 @@
 class Zone < ApplicationRecord
 	#attr_accessor :user_id, :name
 	belongs_to :manager, optional: true
-	has_and_belongs_to_many :users
+	#has_and_belongs_to_many :users
+	has_many :users
+	belongs_to :users
 end
