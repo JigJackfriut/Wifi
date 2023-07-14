6b32ce0fb165957b68863fb2ecd0a65ab1807ff8
michpolak <polak2@kenyon.edu>
2023-07-14 13:12:52 +0000

    Fixed update_wireless_clients API

--- app/controllers/api/v1/wificlients.rb
+++ app/controllers/api/v1/wificlients.rb
@@ -49,7 +49,9 @@ module API
 		end 
 		
 		route :post, 'update_wireless_clients' do 
+			puts "HERE ARE PARAMS FOR WIRELESS, GO LOOK! #{params}"
 			update_wireless_clients(params)
+			
 		end 
       
     end
@@ -206,9 +208,10 @@ end
 
 
 def update_wireless_clients(params)
-	apMac = params[:mac]
+	apMac = params['AP']
 	
-	stations = params[:stations]
+	stations = params['Stations']
+	puts "HERE HERE THIS IS WHAT STATION MAC LOOKS LIKE #{stations}"
 	stations.each do |station|
 		stationParams = station[1]
 		puts "THIS IS WHAT STATION MAC LOOKS LIKE #{stationParams}"
@@ -216,20 +219,20 @@ def update_wireless_clients(params)
 		#puts "#{ap} AND lowercase mac #{station[:mac].downcase}"
 	 
 	if stationTest
-		if stationTest.station != stationParams['mac'].downcase or stationTest.interface != stationParams[:interface] or stationTest.channel != params[:channel] or
-		stationTest.rx_bytes != stationParams[:rx_bytes] or stationTest.tx_bytes != stationParams[:tx_bytes] or stationTest.tx_retries != stationParams[:tx_retries] or stationTest.tx_failed != stationParams[:tx_failed] or 
-		stationTest.signal != stationParams[:signal] or stationTest.signal_avg != stationParams[:signal_avg] or stationTest.tx_bitrate != stationParams[:tx_bitrate] or stationTest.rx_bitrate != stationParams[:rx_bitrate] or 
-		stationTest.expected_throughput != stationParams[:expected_throughput] or stationTest.associated != stationParams[:associated] or stationTest.vid != stationParams[:vid] or stationTest.ssid != stationParams[:ssid] or 
-		stationTest.user_id != stationParams[:user_id] or stationTest.event != stationParams[:event] or stationTest.mac != stationParams[:mac] 
-			stationTest.update(AP: apMac, station: stationParams['mac'].downcase, interface: stationParams[:interface],channel: stationParams[:channel], rx_bytes: stationParams[:rx_bytes], tx_bytes: stationParams[:tx_bytes], tx_retries: stationParams[:tx_retries],tx_failed: stationParams[:tx_failed], 
-							signal: stationParams[:signal], signal_avg: stationParams[:signal_avg], tx_bitrate: stationParams[:tx_bitrate], rx_bitrate: stationParams[:rx_bitrate], expected_throughput: stationParams[:expected_throughput], associated: stationParams[:associated], vid: stationParams[:vid], ssid: stationParams[:ssid],
-							user_id: stationParams[:user_id], event: stationParams[:event], mac: stationParams[:mac])
+		if stationTest.station != stationParams['mac'].downcase or stationTest.interface != stationParams['interface'] or stationTest.channel != stationParams['channel'] or
+		stationTest.rx_bytes != stationParams['rx bytes'] or stationTest.tx_bytes != stationParams['tx bytes'] or stationTest.tx_retries != stationParams['tx retries'] or stationTest.tx_failed != stationParams['tx failed'] or 
+		stationTest.signal != stationParams['signal'] or stationTest.signal_avg != stationParams['signal avg'] or stationTest.tx_bitrate != stationParams['tx bitrate'] or stationTest.rx_bitrate != stationParams['rx bitrate'] or 
+		stationTest.expected_throughput != stationParams['expected throughput'] or stationTest.associated != stationParams['associated'] or stationTest.vid != stationParams['vid'] or stationTest.ssid != stationParams['ssid'] or 
+		stationTest.user_id != stationParams['user_id'] or stationTest.event != stationParams['event'] or stationTest.mac != stationParams['mac'] 
+			stationTest.update(AP: apMac, station: stationParams['mac'].downcase, interface: stationParams['interface'],channel: stationParams['channel'], rx_bytes: stationParams['rx bytes'], tx_bytes: stationParams['tx bytes'], tx_retries: stationParams['tx retries'],tx_failed: stationParams['tx failed'], 
+							signal: stationParams['signal'], signal_avg: stationParams['signal avg'], tx_bitrate: stationParams['tx bitrate'], rx_bitrate: stationParams['rx bitrate'], expected_throughput: stationParams['expected throughput'], associated: stationParams['associated'], vid: stationParams[:vid], ssid: stationParams['ssid'],
+							user_id: stationParams['user_id'], event: stationParams['event'], mac: stationParams['mac'].downcase)
 		end
 	else 
 		#puts "LOOOOOOOOKKK #{station['stations']}"
 		stationTest = StationLog.create(AP: apMac, station: stationParams['mac'].downcase, interface: stationParams['interface'],channel: stationParams['channel'], rx_bytes: stationParams['rx bytes'], tx_bytes: stationParams['tx bytes'], tx_retries: stationParams['tx retries'],tx_failed: stationParams['tx failed'], 
 							signal: stationParams['signal'], signal_avg: stationParams['signal avg'], tx_bitrate: stationParams['tx bitrate'], rx_bitrate: stationParams['rx bitrate'], expected_throughput: stationParams['expected throughput'], associated: stationParams['associated'], vid: stationParams['vid'], ssid: stationParams['ssid'],
-							user_id: stationParams['user id'], event: stationParams['event'], mac: stationParams['mac'].downcase)
+							user_id: stationParams['user_id'], event: stationParams['event'], mac: stationParams['mac'].downcase)
 	end
 		
 	
