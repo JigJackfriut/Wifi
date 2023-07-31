1bb9493198ee814d9e29697082ffa679408a1b29
James Skon <skonjp@kenyon.edu>
2023-07-31 14:24:00 +0000

    The bytes and megabytes

--- app/helpers/station_logs_helper.rb
+++ app/helpers/station_logs_helper.rb
@@ -23,4 +23,24 @@ def secondsToTime(connectedTime)
 	end 
 	return time
 end
+
+def bytes_conversion(bytes)
+if bytes != nil
+   s = ""
+		if (bytes /1073741824) != 0
+      		gigabytes = (bytes / 1073741824).round(2)
+      		s = s + gigabytes.to_s + " GB"
+		elsif (bytes / 1048576) != 0
+      		megabytes = (bytes / 1048576).round(2)
+			s = s + megabytes.to_s + " MB"
+		elsif (bytes / 1024).floor != 0
+			kilobytes = (bytes / 1024).round(2)
+			s = s + kilobytes.to_s + " KB"
+		elsif (bytes/1024) == 0
+			s = s + bytes.to_s + " Bytes"
+  end
+	return s
+end
+end
+
 end #end of module
