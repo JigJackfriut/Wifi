a3fe9e9811c5f87e445fcb9c8d5f08354cbdd9bd
james skon <skonjp@kenyon.edu>
2024-02-04 15:07:00 +0000

    update for rails 3.3.0

--- checkpuma.sh
+++ checkpuma.sh
@@ -1,15 +1,22 @@
+# Script to keep rails running in case it stops
+# Mu edit the crontab in user mode:
+# "crontab -e"
+# then add line:
+# * * * * * /home/skon/cloudwifi/checkpuma.sh
+# Change "skon" to a user with sudo privilege in /etc/group
+# and add to sudoers list
 if pgrep -f cloudwifi | grep ruby >/dev/null
 then
     echo "Puma is running."
 else
     #echo "Puma is not running."
-    sudo su ubuntu
+    sudo su skon
     #echo "Change to cloudwifi directory" >> "/home/ubuntu/cloudwifi/cron.log"
-    cd /home/ubuntu/cloudwifi
-    #echo "Remove log"  >> "/home/ubuntu/cloudwifi/cron.log"
+    cd /home/skon/cloudwifi
+    #echo "Remove log"  >> "/home/skon/cloudwifi/cron.log"
     #rm log/development.log
-    #echo "Start Rails"  >> "/home/ubuntu/cloudwifi/cron.log"
-    export PATH=/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:/home/ubuntu/.local/bin:/home/ubuntu/.rbenv/shims:/home/ubuntu/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+    #echo "Start Rails"  >> "/home/skon/cloudwifi/cron.log"
+    export PATH=/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:/home/skon/.local/bin:/home/skon/.rbenv/shims:/home/skon/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
     nohup rails s -b 0.0.0.0 &
-    #echo "DONE!"  >> "/home/ubuntu/cloudwifi/cron.log"
+    #echo "DONE!"  >> "/home/skon/cloudwifi/cron.log"
 fi
