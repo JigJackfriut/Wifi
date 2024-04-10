ce2e60959421af35c5f66f4c0f4f4079df23ee44
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2024-04-10 19:49:00 -0400

    Update wificlients.rb

--- app/controllers/api/v1/wificlients.rb
+++ app/controllers/api/v1/wificlients.rb
@@ -256,7 +256,7 @@ def update_wireless_clients(params)
     
     existing_record = StationLog.find_by(ap_mac: ap_mac, mac: mac.downcase, interface: station_params['interface'])
     
-    if existing_record.nil? || existing_record.tx_bytes < station_params['tx bytes'].to_i || existing_record.rx_bytes < station_params['rx bytes'].to_i
+    if existing_record.nil? || existing_record.tx_bytes.to_i < station_params['tx bytes'].to_i || existing_record.rx_bytes.to_i < station_params['rx bytes'].to_i
       # Create a new record if no existing record found or if the new data has higher TX or RX bytes
       StationLog.create(ap_mac: ap_mac, mac: mac.downcase, interface: station_params['interface'], channel: station_params['channel'], 
                         rx_bytes: station_params['rx bytes'], tx_bytes: station_params['tx bytes'], tx_retries: station_params['tx retries'], 
@@ -267,6 +267,9 @@ def update_wireless_clients(params)
                         event: station_params['event'], connected_time: station_params['connected time'])
     else
       # Update existing record if the new data has higher TX or RX bytes
+	    if station_params['connected time'] > existing_record.connected_time.to_i 
+		    stationTest= stationTest.update(manager_id: User.find_by(id: stationParams['user_id']).manager_id)
+	    end
       existing_record.update(rx_bytes: station_params['rx bytes'], tx_bytes: station_params['tx bytes'], 
                              tx_retries: station_params['tx retries'], tx_failed: station_params['tx failed'], 
                              signal: station_params['signal'], signal_avg: station_params['signal avg'], 
@@ -275,6 +278,9 @@ def update_wireless_clients(params)
                              associated: station_params['associated'], vid: station_params['vid'], ssid: station_params['ssid'], 
                              user_id: station_params['user_id'], event: station_params['event'], 
                              connected_time: station_params['connected time'])
+	    
     end
   end
 end
+
+def update_managers
