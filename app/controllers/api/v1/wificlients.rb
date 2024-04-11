2121356852678695701d5e990f85ce5884133bed
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2024-04-11 15:42:51 -0400

    Update wificlients.rb

--- app/controllers/api/v1/wificlients.rb
+++ app/controllers/api/v1/wificlients.rb
@@ -276,15 +276,7 @@ def update_wireless_clients(params)
                                associated: station_params['associated'], vid: station_params['vid'], ssid: station_params['ssid'],
                                user_id: station_params['user_id'], event: station_params['event'],
                                connected_time: station_params['connected time'],current_time: station_params['current time'])
-      else
-	            StationLog.create(ap_mac: ap_mac, mac: mac.downcase, interface: station_params['interface'], channel: station_params['channel'],
-                        rx_bytes: station_params['rx bytes'], tx_bytes: station_params['tx bytes'], tx_retries: station_params['tx retries'],
-                        tx_failed: station_params['tx failed'], signal: station_params['signal'], signal_avg: station_params['signal avg'],
-                        tx_bitrate: station_params['tx bitrate'], rx_bitrate: station_params['rx bitrate'],
-                        expected_throughput: station_params['expected throughput'], associated: station_params['associated'],
-                        vid: station_params['vid'], ssid: station_params['ssid'], user_id: station_params['user_id'],
-                        event: station_params['event'], connected_time: station_params['connected time'],current_time: station_params['current time'])
-	      
+      
       end
     end
 
