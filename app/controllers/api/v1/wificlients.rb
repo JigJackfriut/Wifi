7a71991a9706364be8c960ce88a72ce0c0bdef97
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2024-04-11 13:48:42 -0400

    Update wificlients.rb

--- app/controllers/api/v1/wificlients.rb
+++ app/controllers/api/v1/wificlients.rb
@@ -278,5 +278,13 @@ def update_wireless_clients(params)
                                connected_time: station_params['connected time'])
       end
     end
+	  timestamp_seconds = station_params['connected time'].to_i /1000
+	    timestamp_time = Time.at(timestamp_seconds)
+	    current_time = Time.now
+	    difference_in_seconds = (current_time - timestamp_time).to_i
+	    if difference_in_seconds < 180
+		  stationTest.update(manager_id: User.find_by(id: stationParams['user_id']).manager_id)  
+	    end
+
   end
 end
