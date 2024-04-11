85d837f5f01babd88c315dfce7391bd616335d6a
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2024-04-11 14:31:51 -0400

    Update wificlients.rb

--- app/controllers/api/v1/wificlients.rb
+++ app/controllers/api/v1/wificlients.rb
@@ -253,7 +253,15 @@ def update_wireless_clients(params)
   stations.each do |station|
     station_params = station[1]
     mac = station[0]
-
+    timestamp_seconds = station_params['connected time'].to_i / 1000
+    timestamp_time = Time.at(timestamp_seconds)
+    current_time = Time.now
+    difference_in_seconds = (current_time - timestamp_time).to_i
+    if difference_in_seconds < 180 && station_params['user_id'].present?
+          stationTest.update(manager_id: User.find_by(id: station_params['user_id'])&.manager_id)
+    else 
+	   stationTest.destroy if stationTest.present? 
+    end
     existing_record = StationLog.find_by(ap_mac: ap_mac, mac: mac.downcase, interface: station_params['interface'])
 
     if existing_record.nil?
@@ -276,25 +284,18 @@ def update_wireless_clients(params)
                                associated: station_params['associated'], vid: station_params['vid'], ssid: station_params['ssid'],
                                user_id: station_params['user_id'], event: station_params['event'],
                                connected_time: station_params['connected time'])
+      else
+	            StationLog.create(ap_mac: ap_mac, mac: mac.downcase, interface: station_params['interface'], channel: station_params['channel'],
+                        rx_bytes: station_params['rx bytes'], tx_bytes: station_params['tx bytes'], tx_retries: station_params['tx retries'],
+                        tx_failed: station_params['tx failed'], signal: station_params['signal'], signal_avg: station_params['signal avg'],
+                        tx_bitrate: station_params['tx bitrate'], rx_bitrate: station_params['rx bitrate'],
+                        expected_throughput: station_params['expected throughput'], associated: station_params['associated'],
+                        vid: station_params['vid'], ssid: station_params['ssid'], user_id: station_params['user_id'],
+                        event: station_params['event'], connected_time: station_params['connected time'])
+	      
       end
     end
     
   end
 end
 
-def update_manager_ids_for_recently_connected_stations
-  StationLog.where("connected_time > ?", 3.minutes.ago).each do |station_log|
-    next unless station_log.user_id.present?
-
-    timestamp_seconds = station_log.connected_time.to_i / 1000
-    timestamp_time = Time.at(timestamp_seconds)
-    current_time = Time.now
-    difference_in_seconds = (current_time - timestamp_time).to_i
-
-    if difference_in_seconds < 180
-      station_log.update(manager_id: User.find_by(id: station_log.user_id)&.manager_id)
-    else
-      station_log.destroy
-    end
-  end
-end
