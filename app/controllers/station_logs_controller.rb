592c3ccceec6e4499ee152ce5376560710511262
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2024-04-16 23:38:06 -0400

    Update station_logs_controller.rb

--- app/controllers/station_logs_controller.rb
+++ app/controllers/station_logs_controller.rb
@@ -276,10 +276,7 @@ class StationLogsController < ApplicationController
       format.json { head :no_content }
     end
   end
-	def time
-		@current_time = Time.now
-
-	end
+	
 
   private
     # Use callbacks to share common setup or constraints between actions.
