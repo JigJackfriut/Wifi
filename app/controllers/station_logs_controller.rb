babe444e3bafa225bfd843f443d98854ce1cd6da
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2024-04-17 19:40:43 -0400

    Update station_logs_controller.rb

--- app/controllers/station_logs_controller.rb
+++ app/controllers/station_logs_controller.rb
@@ -14,7 +14,9 @@ class StationLogsController < ApplicationController
 	current_time= current_time.slice(0...-3)
     current_time= current_time.to_i
     comp_time= real_time-current_time
-    if comp_time > 180000
+    if comp_time < 180000
+      record.update(associated: 'yes')
+    else
       record.update(associated: 'no')
     end
   end
