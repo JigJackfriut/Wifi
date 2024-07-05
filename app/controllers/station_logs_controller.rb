0976412d7bb7d7e1469d2a7518170a67d0bf4b50
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2024-07-05 13:16:11 -0400

    Update station_logs_controller.rb

--- app/controllers/station_logs_controller.rb
+++ app/controllers/station_logs_controller.rb
@@ -3,8 +3,6 @@ class StationLogsController < ApplicationController
   before_action :set_station_log, only: %i[ show edit update destroy ]
   before_action :authenticate_manager!
   before_action :current_manager, only: [:edit, :update, :destroy]
-  validates :name, presence: true, length: { maximum: 50 }
-
 
   # GET /station_logs or /station_logs.json
   def index
@@ -294,15 +292,6 @@ end
       format.json { head :no_content }
     end
   end
-
-  def update_name
-    @user = StationLog.find_by(mac: params[:mac])
-    if @user.update(name_params)
-      redirect_to active_users_path, notice: 'Name updated successfully.'
-    else
-      redirect_to active_users_path, alert: 'Failed to update name.'
-    end
-  end
 	
 
   private
@@ -313,6 +302,6 @@ end
 
     # Only allow a list of trusted parameters through.
     def station_log_params
-      params.require(:station_log).permit(:AP, :station, :interface, :channel, :rx_bytes, :tx_bytes, :tx_retries, :tx_failed, :signal, :signal_avg, :tx_bitrate, :rx_bitrate, :expected_throughput, :associated, :vid, :ssid, :user_id, :event, :mac,:name)
+      params.require(:station_log).permit(:AP, :station, :interface, :channel, :rx_bytes, :tx_bytes, :tx_retries, :tx_failed, :signal, :signal_avg, :tx_bitrate, :rx_bitrate, :expected_throughput, :associated, :vid, :ssid, :user_id, :event, :mac)
     end
 end
